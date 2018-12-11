c91d5ed608d6ed7ae3bc7b9b6c5ff4f1c7c604cc
Abraham Bueno <abueno@localhost.localdomain>
2018-12-11 14:42:26 -0600

    A better approach to solving the problem

--- 1209.LongestSubstringWithoutRepeatingCharacters.js
+++ 1209.LongestSubstringWithoutRepeatingCharacters.js
@@ -3,100 +3,112 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    const arr1 = [];
-    const arr2 = [];
-    const tiedArr = [];
-    let current = 's1'
-    
+    const string1 = [];
+    const string2 = [];
+    let current = 's1';
     for (let i = 0; s.length > i; i++) {
         if (i === 0) {
-            arr1.push(s[i]);
+            string1.push(s[i]);
         }
-        const lastArr1 = (arr1.length - 1);
-        const lastArr2 = (arr2.length - 1);
-        
-        if (s[i] !== arr1[lastArr1] && current === 's1') {
-            arr1.push(s[i])
-        }
-        if (s[i] !== arr2[lastArr2] && current === 's2') {
-            arr2.push(s[i])
-        }
-        // console.log('looping... arr2[lastArr2]', arr2[lastArr2], arr2, 'arr1[lastArr1]', arr1[lastArr1], arr1[arr1.length - 1])
-        if (s[i] === arr2[lastArr2] && i !== 0 || s[i] === arr1[lastArr1] && i !== 0){
+        if (i !== 0){
             if (current === "s1") {
-                current = "s2";
-                console.log('switch to s2', 'last', s[i - 1], 'current', s[i], i)
-                arr2.push(s[i]);
-            } else {
-                current = "s1";
-                console.log('switch to s1', 'last', s[i - 1], 'current', s[i], i)
-                arr1.push(s[i]);
-            }
-        }
-    };
-    console.log('end of loop 1', arr1, arr2)
-    
-    // loop in reverse to find longest string in opposite direction
-    const temp = [];
-    const lastTemp = temp.length - 1;
-    for (let i = (s.length - 1); i >= 0; i--) {
-        if (i === (s.length - 1)) {
-            temp.push(s[i])
-        };
-        if (s[i] !== temp[lastTemp]) {
-            temp.push(s[i])
-        }
-        if (s[i] === temp[lastTemp]) {
-            const length1 = arr1.length;
-            const length2 = arr2.length;
-            const tempLength = temp.length;
-            let smaller;
-            if (length2 > length1) {
-                smaller = "a1";
-            }
-            if (length1 > length2) {
-                smaller = "a2";
-            }
-            if (smaller === "a1") {
-                if (temp.length > arr1.length) {
-                    let tracker = arr1.length - 1;
-                    while (arr1[tracker] !== undefined){
-                        arr1.pop();
-                    } 
-                    temp.forEach(each => {
-                        arr1.push(each)
-                    })
-                } else {
-                    let tracker = temp.length - 1;
-                    while (temp[tracker] !== undefined) {
-                        temp.pop();
-                    } 
-                    temp.push(s[i])
+             let tracker = 0;
+                while (string1[tracker] !== undefined) {
+                    if (string1[tracker] === s[i]) {
+                        if (string2[0] !== undefined) {
+                            if (string1.length > string2.length) {
+                                let inTrack = string2.length - 1;
+                                while (string2[inTrack] !== undefined) {
+                                    string2.pop();
+                                    inTrack--
+                                }
+                                current = "s2";
+                                // string2.push(s[i]);
+                                console.log('switch to s2', current, 'emptied', string2)
+                            } else if (string2.length > string1.length) {
+                                let inTrack = string1.length - 1;
+                                while (string1[inTrack] !== undefined) {
+                                    string1.pop();
+                                    inTrack--
+                                }
+                                current = "s1";
+                                // string1.push(s[i]);
+                                console.log('switch to s1', current, 'emptied', string1)
+                            } else {
+                                 let inTrack = string1.length - 1;
+                                while (string1[inTrack] !== undefined) {
+                                    string1.pop();
+                                    inTrack--
+                                }
+                                current = "s1";
+                                // string1.push(s[i]);
+                                console.log('switch to s1', current, 'emptied', string1)
+                            }
+                            
+                        } else {
+                            current = 's2';
+                            // string2.push(s[i]);
+                            console.log('switch to s2', current)
+                        }
+                        
+                    }
+                    tracker++;
                 }
-            };
-            if (smaller === "a2") {
-                if (temp.length > arr2.length) {
-                    let tracker = arr2.length - 1;
-                    while (arr2[tracker] !== undefined) {
-                        arr2.pop();
-                    } 
-                    temp.forEach(each => {
-                        arr2.push(each)
-                    });
-                    while (temp[tracker] !== undefined) {
-                        temp.pop();
-                    } 
-                } else {
-                    let tracker = temp.length - 1;
-                    while (temp[tracker] !== undefined) {
-                        temp.pop();
-                    } 
-                    temp.push(s[i])
+                if (string1[tracker] === undefined && current === "s1") {
+                    console.log('what is current', current, 'value of element ', s[i])
+                    string1.push(s[i]);
                 }
-                
+            }
+            if (current === "s2") {
+                let tracker = 0;
+                while (string2[tracker] !== undefined) {
+                    if (string2[tracker] === s[i]) {
+                        if (string1.length > string2.length) {
+                            let inTrack = string2.length - 1;
+                            while (string2[inTrack] !== undefined) {
+                                string2.pop();
+                                inTrack--
+                            }
+                            current = "s2";
+                            string2.push(s[i]);
+                            console.log('switch to s2', current, 'emptied', string2)
+                        } else if (string2.length > string1.length) {
+                            let inTrack = string1.length - 1;
+                            while (string1[inTrack] !== undefined) {
+                                string1.pop();
+                                inTrack--
+                            }
+                            current = "s1";
+                            string1.push(s[i]);
+                            console.log('switch to s1', current, 'emptied', string1)
+                        } else {
+                            let inTrack = string1.length - 1;
+                            while (string1[inTrack] !== undefined) {
+                                string1.pop();
+                                inTrack--;
+                            }
+                            current = "s1";
+                            string1.push(s[i]);
+                            console.log('switch to s1', current, string1, 'emptied', string1)
+                            
+                        }
+                        // current = 's1';
+                        // string1.push(s[i]);
+                    }
+                    tracker++;
+                }
+                if (string2[tracker] === undefined && current === 's2') {
+                    string2.push(s[i]);
+                }
             }
         }
+        console.log('loop', ' i ', i, 'string1 ', string1, ' string2 ', string2);
     }
-    console.log('arr1', arr1,'arr2', arr2, temp)
-    
+    console.log('end', string1, string2)
+    if (string1.length === string2.length) return string2.length;
+    if (string1.length > string2.length) {
+      return string1.length  
+    } else {
+        return string2.length;
+    } 
 };
