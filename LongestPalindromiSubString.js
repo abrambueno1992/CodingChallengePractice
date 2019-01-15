15e4bbd54c7ef003dcebcc81e90a808c8ceb7f7f
Abraham Bueno <abueno@localhost.localdomain>
2019-01-15 16:23:53 -0600

    Work in progress

--- LongestPalindromiSubString.js
+++ LongestPalindromiSubString.js
@@ -3,52 +3,46 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    const sStr = s.split('');
-    console.log("String is ", sStr)
-    const lgStr = [];
-    let shStr = "";
+    let origStr = s.split("");
+    if (origStr.length === 1) return s[0]
+    const trackValues = new Map();
+    
     let tracker = 0;
-    while (tracker !== sStr.length - 1) {
-        let curChar = "";
-        for (let i = tracker; sStr.length > i; i++) {
-            if (i === tracker) {
-                curChar = sStr[tracker]
-            }
-            // console.log("Current character is ", curChar)
-            
+    while (origStr[tracker] !== undefined) {
+        let curChar = origStr[tracker];
+        for (let i = tracker; origStr.length > i; i++) {
             if (i !== tracker) {
-                if (curChar === sStr[i]) {
-                    console.log("Matching... curChar: ", curChar, " sStr[i]: ", sStr[i])
-                    const diff = i - tracker;
-                    
-                    
-                    if (lgStr[0] === undefined) {
-                            lgStr.push(diff)
+                if (curChar === origStr[i]) {
+                    let lengthStr = i - tracker;
+                    lengthStr++;
+                    let strCompare = "";
+                    for (let j = tracker; i >= j; j++) {
+                        strCompare += origStr[j];
                     }
-                    const last = lgStr.length - 1;
-                    console.log("last: ", last, " diff: ", diff)
                     
-                    
-                    if (lgStr[last] < diff || shStr === "") {
-                        console.log("Condition met")
-                        shStr = "";
-                        for (let j = tracker; i >= j; j++) {
-                            shStr += sStr[j]
-                        }
-                        lgStr.push(diff);
-                        lgStr.sort();
-                    }
-                } else {
-                    
+                    trackValues.set(lengthStr, strCompare)
                 }
             }
-            
-            
         }
         tracker++;
     }
-    return shStr;
     
+    let longestString = "";
+    let currentMax = 0;
+    trackValues.forEach((key, value) => {
+        if (value > currentMax) {
+            currentMax = value;
+            longestString = key;
+        }
+        
+        console.log('key: ', key, " value: ", value, ' length', value.length);
+    })
+    
+    console.log('trackValues: ', trackValues);
+    if (trackValues === new Map()) {
+        return origStr[0]
+    }
+    return longestString
     
     
 };
