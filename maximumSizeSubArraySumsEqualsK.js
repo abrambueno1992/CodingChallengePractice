549bcd7b7bff8eceb14ffc046bb29007f6b3b188
Abraham Bueno <abueno@localhost.localdomain>
2019-01-15 13:00:29 -0600

    Solved max size subarray problem

--- maximumSizeSubArraySumsEqualsK.js
+++ maximumSizeSubArraySumsEqualsK.js
@@ -4,80 +4,27 @@
  * @return {number}
  */
 var maxSubArrayLen = function(nums, k) {
-    const arrNums = [];
+    let sum=0;
+    let sumMap=new Map();
     
-    // tracking
-    let track = 0;
-    let sumTrack = 0;
-    
-    // current sum
-    
-    const tempPairs = [];
-    if (nums[0] === k) return 1
-    
-    while (nums[track] !== undefined) {
-        let tempSum = 0;
-        for (let i = track; nums.length > i; i++) {
-            if (i !== track) {
-                
-                if (i - 1 === track) {
-                    tempSum = nums[track] + nums[i];
-                    console.log(track,"initial addition: ", tempSum)
-                } else {
-                    tempSum += nums[i]
-                }
-            }
-        }
-        for (let i = nums.length - 1; i >= track; i--) {
-            let summed = 0;
-            console.log('tempSum: ', tempSum)
-            if (i !== track) {
-                if (tempSum === k) {
-                    let result = i - track;
-                    result++;
-                    const tempA = [];
-                    for (let j = track; i > j; j++) {
-                        tempA.push(nums[j]); 
-                    }
-                    arrNums.push(tempA);
-                    tempPairs.push(result);
-                    // break;
-                    // return result;
-                } else {
-                    summed = tempSum - nums[i];
-                }
-                if (summed === k) {
-                    let result = i - track;
-                    result++;
-                    const tempA = [];
-                    for (let j = track; i > j; j++) {
-                        tempA.push(nums[j]); 
-                    }
-                    arrNums.push(tempA);
-                    tempPairs.push(result);
-                    
-                    // break;
-                    // return result;
-                }
-            }
-        }
+    // key 0 === -1
+    sumMap.set(0,-1);
+
+    let maxSumSubArrayLen =0;
+
+    for(let i=0; i<nums.length; i++){
+        sum+=nums[i];
+
+        if(!sumMap.has(sum)) sumMap.set(sum,i);
         
-        track++;
-    }
-    tempPairs.sort();
-    console.log('final array', tempPairs)
-    arrNums.sort();
-    console.log("all arrays: ", arrNums)
-    let largest =-10;
-    arrNums.forEach((each, i) => {
-        if (each.length > largest) {
-            largest = each.length
+        // if (sum - k) === 0, sumMap has key 0 === -1
+        if(sumMap.has(sum-k)){
+            // sumMap.get(0) === - 1
+            // Math.max(currentMax, i -- 1)
+            maxSumSubArrayLen = Math.max(maxSumSubArrayLen, i-sumMap.get(sum-k));
         }
-    })
-    if (largest > 0) {
-        return largest
     }
-    return 0;
-    // return tempPairs[tempPairs.length - 1];
+
+    return maxSumSubArrayLen;
     
 };
