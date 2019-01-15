1888bcb708ba7e55932891c5ffbd17c07759071f
Abraham Bueno <abueno@localhost.localdomain>
2019-01-15 15:40:56 -0600

    Completed challenge

--- CompareVersionNumbers.js
+++ CompareVersionNumbers.js
@@ -8,24 +8,16 @@ var compareVersion = function(version1, version2) {
     let num2 = version2.split(".");
     const largest = (num.length > num2.length) ? num : num2;
     for (let i = 0; largest.length > i; i++) {
-        let n1 = parseInt(num[i], 10);
-        let n2 = parseInt(num2[i], 10);
-        console.log("Each value.. v1: ",num[i], " v2: ", num2[i])
+        let n1 = (num[i]) ? parseInt(num[i], 10) : 0;
+        let n2 = (num2[i]) ? parseInt(num2[i], 10) : 0;
         
-        if (n2 > n1 || (num[i] === undefined && num2[i] !== undefined)) {
-            if (n2 === 0 && i === largest.length - 1) {
-                return 0;
-            }
+        if (n2 > n1 ) {
             return -1;
         }
-        if (n1 > n2 || (num2[i] === undefined && num[i] !== undefined)) {
-            if (n1 === 0 && i === largest.length - 1) {
-                return 0;
-            }
+        if (n1 > n2 ) {
             return 1;
         }
     }
     return 0;
-    console.log(num, num2, largest)
     
 };
