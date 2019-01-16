5be5aafcd6a8e3dfbe74143027172f87886d302f
Abraham Bueno <abueno@localhost.localdomain>
2019-01-15 23:52:17 -0600

    LOL lots of code.

--- IntegerToEnglishWords.js
+++ IntegerToEnglishWords.js
@@ -13,6 +13,42 @@ var numberToWords = function(num) {
     let currentLength = lArr;
     
     let finalString = "";
+    const one = "one";
+    const two = "two";
+    const three = "three";
+    const four = "four";
+    const five = "five";
+    const six = "six";
+    const seven = "seven";
+    const eight = "eight";
+    const nine = "nine";
+    
+    const appendPairs = (three, resPair) => {
+        if (three === true) {
+            const splitPairs = resPair;
+            splitPairs.shift();
+            let retString3 = "";
+            const num1 = parsetInt(splitPairs[0], 10);
+            const num2 = parsetInt(splitPairs[1], 10);
+            if (num1 === 1 && num2 === 0) {
+                retString3 += "ten"
+            } else if (num1 === 1 && num2 === 0){
+                retString3 += ""
+            } else if (num1 === 1 && num2 === 0) {
+                
+            } else if (num1 === 1 && num2 === 0) {
+                
+            } else (num1 === 1 && num2 >= 14) {
+                
+            }
+        } else {
+            const splitPairs = resPair;
+            splitPairs.shift();
+            splitPairs.shift();
+            let retString2 = "";
+            const num1 = parsetInt(splitPairs[0], 10);
+        }
+    }
     
     // number pair of max length of 3... 1,235,555
     // index is the highest in the number pair
@@ -20,26 +56,33 @@ var numberToWords = function(num) {
     // index === 7, index === 6, index === 3
     const appendTo = (index, pairN) => {
         let quantity;
-        if (index === 9) {
+        if (index >= 13  && index <= 15) {
+            quantity = "trillion"
+        } else if (index >= 10  && index <= 12) {
             quantity = "billion"
-        } else if (index === 8) {
+        } else if (index >= 7  && index <= 9) {
             quantity = "million"            
-        } else if (index === 7) {
-            
-        } else if (index === 6) {
-            
-        } else if (index === 5) {
-            
-        } else if (index === 4) {
-            
+        } else if (index >= 4  && index <= 6) {
+            quantity = "thousand"
         } else if (index === 3) {
-            
+            quantity = "hundred"
         } else if (index === 2) {
-            
+            quantity = "lol ten to ninety"
         } else if (index === 1) {
+            quantity = "one"
+        } else {
             
         }
-        console.log('function called, index:', index, " pair:", pairN)
+        // check length of pair
+        if (pairN.split("") === 3) {
+            appendPairs(true, pairN.split(""))
+        } else if (pairN.split("") === 2) {
+            appendPairs(false, pairN.split(""))
+        } else {
+            
+        }
+    
+        console.log('function called, index:', index, " pair:", pairN, " quantity:", quantity)
         
     }
     
