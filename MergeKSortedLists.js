cba4e1ec7554b8e487ccb3531b2e0180c6856eda
Abraham Bueno <abueno@localhost.localdomain>
2019-01-17 20:18:38 -0600

    REviewed solution, interesting.

--- MergeKSortedLists.js
+++ MergeKSortedLists.js
@@ -9,41 +9,32 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
-var mergeKLists = function(lists) {
-    let tracker = 0;
-    let lArr= lists[tracker];
-    const lValues = [];
-    if (lists.length === 0 || lists[0] === undefined || lists === [] || !lists) {
-        return lists
-    }
-    
-    const loopNodes = lArr => {
-        while (lArr) {
-            console.log(lArr.val);
-            lValues.push(lArr.val);
-            lArr = lArr.next;
-        }
-        tracker++;
-        lArr = lists[tracker];
-        if (lArr) {
-            loopNodes(lArr);
-        }
-    }
-    loopNodes(lArr)
-    lValues.sort((a, b) => a - b)
-    
-    console.log('Values are:', lValues);
-    let newNode = new ListNode(lValues[0]);
-    // newNode = newNode.next;
-    let nNode = newNode;
-    let trackNew = 1;
-    while (lValues[trackNew] !== undefined) {
-        nNode.next = new ListNode(lValues[trackNew]);
-        nNode = nNode.next
-        trackNew++;
-    }
-    // if (newNode[0] === undefined) {
-    //     return lists
-    // }
-    return newNode;
+
+
+const mergeTwoLists = (l1, l2) => {
+	if (l1 === null) return l2
+	if (l2 === null) return l1
+	let newList = null
+
+	if (l1.val > l2.val) {
+		newList = new ListNode(l2.val)
+		newList.next = mergeTwoLists(l1, l2.next)
+	} else {
+		newList = new ListNode(l1.val)
+		newList.next = mergeTwoLists(l1.next, l2)
+	}
+	return newList
+}
+
+var mergeKLists = function(lists, lo = 0, hi = lists.length - 1) {
+    if (lists.length === 0) return null
+	if (lo === hi) return lists[lo] // return a single list to merge later
+	let mid = Math.floor((lo + hi) / 2)
+	let left = mergeKLists(lists, lo, mid) // merge all lists on the left side 
+	let right = mergeKLists(lists, mid + 1, hi) // merge all lists on the right side 
+
+	return mergeTwoLists(left, right)
+
 };
+
+
