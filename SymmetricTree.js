d1955ff3d10bec19c224f9678f922f3fd845912e
Abraham Bueno <abueno@localhost.localdomain>
2019-01-21 20:48:30 -0600

    Symmetric tree solved

--- SymmetricTree.js
+++ SymmetricTree.js
@@ -10,71 +10,17 @@
  * @return {boolean}
  */
 var isSymmetric = function(root) {
-    let nodeTrack = root;
-    if (!root) return null;
-    const rootVal = root.val
-    const lNodes = [];
-    const rNodes = [];
-    const aNodes = [];
-    const findNodes = (node, min = Number.NEGATIVE_INFINITY, max = Number.POSITIVE_INFINITY) => {
-        if (node) {
-            aNodes.push(node.val);
-            findNodes(node.left);
-            findNodes(node.right);
-        }
-    }
-    
-    let left = findNodes(root.left);
-    let lengthL = 0;
-    const leftNodes = [];
-    if (aNodes.length !== 0) {
-        lengthL = aNodes.length;
-        let trackI = 0;
-        while (aNodes[trackI] !== undefined) {
-            leftNodes.push(aNodes.shift());
-        }
-    }
-    
-    let right = findNodes(root.right)
-    let lengthR = 0;
-    const rightNodes = [];
-    if (aNodes.length !== 0) {
-        console.log('right nodes', aNodes)
-        lengthR = aNodes.length;
-        let trackI = 0;
-        while (aNodes[trackI] !== undefined) {
-            rightNodes.push(aNodes.shift());
-        }
-    }
-    console.log('all nodes', leftNodes, rightNodes);
-    if (rightNodes[0] === undefined && leftNodes[0] === undefined) {
-        if (root) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    if ((rightNodes.length === leftNodes.length)) {
-        console.log('left length', leftNodes.length, ' righ length', rightNodes.length)
-        if (leftNodes.length % 2 === 0 && rightNodes.length === 1) {
-            return true;
-        } else if (leftNodes.length >= 3) {
-            if (leftNodes.lenght % 2 !== 0) {
-                return true
-            }
-            // return false;
-        } else {
-            return false
-        }
-        
-    } 
-//     else if ((rightNodes.length === leftNodes.length) && (leftNodes.length === 0)) {
-        
-//         return true
-//     } 
-    else {
-        return false;
-    }
-    
-    
+    // An empty tree is balanced by default
+  if (!root) return true;
+  const symmHelp = (left, right) => {
+    if (!left || !right) return left === right
+    if (left.val !== right.val) return false
+      // symmetry
+      // left node.left, right node.right, left node.right right node.left
+      // symmetrical left and right nodes are mirror
+    return symmHelp(left.left, right.right) && symmHelp(left.right, right.left)
+  }
+  
+  return symmHelp(root.left, root.right)
+
 };
