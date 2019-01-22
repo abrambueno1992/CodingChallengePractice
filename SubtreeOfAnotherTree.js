e370084ff1ab69cb78ed6903498a47e3081ecb45
Abraham Bueno <abueno@localhost.localdomain>
2019-01-22 17:31:54 -0600

    Problem solved

--- SubtreeOfAnotherTree.js
+++ SubtreeOfAnotherTree.js
@@ -10,41 +10,24 @@
  * @param {TreeNode} t
  * @return {boolean}
  */
+var isEqual= function(s, t) {
+    // end, both null, true
+    if(!s&&!t) return true;
+    // s && t !== null
+    // s.val === t.val
+    // isEqual(left) returns true && isEqual(right) returns true
+    // if true on left and right, returns true;
+    // if either left or right is false, return false
+    if(s&&t&&s.val===t.val) return isEqual(s.left,t.left)&&isEqual(s.right,t.right);
+    return  false;
+}
 var isSubtree = function(s, t) {
-    let nodeTrack = s;
-    const traverse = (tree, orT) => {
-        let origN = orT;
-        let newN = tree;
-        // while (tree) {
-            if (!origN && newN) return;
-        if (!newN && origN) return;
-        if (!origN && !newN) return true;
-            if (tree.val !== origN.val) {
-                return;
-            }
-        console.log('iter, val:', tree.val)
-        traverse(newN.left, orT);
-        traverse(newN.right, orT);
-        
-            
-        // }
-    }
-    const checkValue = node => {
-        if (!node && !t) return true;
-        if (!node && t) return false;
-        if (!t && node) return false;
-        if (node) {
-            
-            console.log('current node: ', node.val, 'node:', node.val, 't:', t.val)
-            traverse(node.left, t);
-            traverse(node.right, t);
-        }
-    }
-    if (!s) return null;
-    if (s === t) {
-        return true;
-    }
-    checkValue(nodeTrack);
-    // checkValue(nodeTrack.right);
+    // end, both null, true
+    if(!s&&!t) return true;
+    // if isEqual(s,t) returns true, then it's true;
+    if(isEqual(s,t)) return true;
+    // Go through left nodes first, until the end or it return true
+    // Go through right nodes next, until the end of it or it returns true;
+    else if (s) return isSubtree(s.left,t)||isSubtree(s.right,t);
     return false;
 };
