70b6b66f24f6d589fb3864b7e72b6dbcd00adac9
Abraham Bueno <abueno@localhost.localdomain>
2019-01-22 00:06:43 -0600

    Solved, reviewed

--- SumRootToLeafNumbers.js
+++ SumRootToLeafNumbers.js
@@ -10,30 +10,19 @@
  * @return {number}
  */
 var sumNumbers = function(root) {
-    const start = root.val;
-    let index = 0;
-    const sums = [];
-    sums.push(start);
-    const checkValues = (nodes, track) => {
-        if (sums[track] === undefined) {
-            if (nodes) {
-               sums[track] =(nodes.val); 
-            }
-            
-        } else if (nodes){
-            // let newSum = sums[index] + nodes.val;
-            sums[track] += nodes.val;
-            // index++
-            checkValues(nodes.left, track + 1)
-            checkValues(nodes.right, track + 2);
-        } else {
-            
+    let result = 0
+    dfs(root, "")
+    
+    function dfs(root, currentPath){
+        if(!root) return
+        if(!root.left && !root.right){
+            currentPath += root.val
+            result += parseInt(currentPath)
+            return
         }
-        
-        
-        
+        dfs(root.left, currentPath + root.val)
+        dfs(root.right, currentPath + root.val)
     }
-    checkValues(root.left, index + 1) && checkValues(root.right, index + 2);
-    console.log('result: ', sums)
-    return sums
+    
+    return result
 };
