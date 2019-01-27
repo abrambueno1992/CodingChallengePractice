dd2fd53b8f72da860076cebdbc56c8f281cb2b86
Abraham Bueno <abueno@localhost.localdomain>
2019-01-26 23:37:32 -0600

    Better notes.

--- NumberOfIslands.js
+++ NumberOfIslands.js
@@ -12,10 +12,12 @@ var numIslands = function(grid) {
         let current = 0;
         for (let i = 0; grid[tracker].length > i;i++) {
             if(grid[tracker][i] === "1") {
+                // current reassigned grid[trakcer][i] ="2"
                 grid[tracker][i] = "2";
+                // recursive
                 isLand(grid, tracker, i );
-        count++;
-    }
+                count++;
+            }
             
         }
         tracker++;
@@ -25,13 +27,20 @@ var numIslands = function(grid) {
     
 };
 function isLand(matrix, i, j) {
- checkXY(matrix, i, j+1); // right
- checkXY(matrix, i-1, j); // top
- checkXY(matrix, i, j-1); // left
- checkXY(matrix, i+1, j); // bottom
+    // recursion, goes through all paths
+    // i vertical, j horizontal
+    // i++ down, i-- up, j++ right, j-- left
+    checkXY(matrix, i, j+1); // right
+    checkXY(matrix, i, j-1); // left
+    checkXY(matrix, i-1, j); // top
+    checkXY(matrix, i+1, j); // bottom
 }
 
 function checkXY(matrix, i, j) {
+    // if matrix[i] is not undefined / defined
+    // and matrix[i][j] === "1"
+    // matrix[i][j] = "0", reassigned
+    // call isLand(matrix, i, j) => recursion
  if(matrix[i] && matrix[i][j] == "1") {
   matrix[i][j] = "0";
   isLand(matrix, i, j);
