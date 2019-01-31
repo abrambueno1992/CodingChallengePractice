0863416be695b8eeb7ee34c3f4d994f98cc3bb36
Abraham Bueno <abueno@localhost.localdomain>
2019-01-30 22:39:06 -0600

    Imrpved and added comments to explain

--- TwoSumIV.js
+++ TwoSumIV.js
@@ -14,38 +14,42 @@ var findTarget = function(root, k) {
     const nums = [];
     let trackNode = root;
     const checkValues = node => {
+        // if node is NOT null
        if (node) {
+           // push root value to nums
            nums.push(node.val);
+           // recursively call checkValues with left nodes
            checkValues(node.left) 
+           // recursively call checkValues with right nodes
            checkValues(node.right);
-       } else {
-           return;
-       }
-        
+       } 
     }
     
-    
-    
-    checkValues(trackNode.left)
-    checkValues(trackNode.right);
+    // push root value to nums
     nums.push(root.val)
+    // recursively go through left nodes
+    checkValues(trackNode.left)
+    // recursively go through right nodes
+    checkValues(trackNode.right);
+    
     let tracker = 0;
-    // console.log("FIRST: ", nums[tracker], nums[0], nums[1], nums)
     while (nums[tracker] !== undefined) {
+        // value at index tracker
         let current = nums[tracker];
         for (let i = tracker + 1; nums.length > i; i++) {
+            // sum of current value and value at i index
             let newSum = current + nums[i];
-            // console.log('newSum: ', newSum, ' tracker:', tracker)
+            // if newSum is equal to k, solution found
             if (newSum === k) {
                 return true;
             }
         }
+        // increment tracker
         tracker++;
     }
-    // console.log('nums: ', nums);
+    // return false, if newSum !== k for all values
     return false
     
-    return nums;
     
     
 };
