aa31717a66d44d0d96513e20e383031c0e66af7a
Abraham Bueno <abueno@localhost.localdomain>
2019-01-31 11:18:04 -0600

    Reviewed and made comments more comprehensible.

--- BinaryTreeVerticalOrderTraversal.js
+++ BinaryTreeVerticalOrderTraversal.js
@@ -13,15 +13,14 @@ var verticalOrder = function(root) {
     if (!root) return [];
     // push all nodes into myQ, as an array
     let myQ = [[root, 0]], myMap = {};
-    console.log("initial myQ: ", myQ)
     
     while (myQ.length !== 0) {
         // remove nodes from the front
         // after shift, myQ is empty
         let [node, i] = myQ.shift();
+        // push the values at index to myMap
+        // node value saved in myMap[i] index
         myMap[i] === undefined ? myMap[i] = [node.val] : myMap[i].push(node.val);
-        console.log('myMap: ', myMap);
-        console.log('myQ: ', myQ);
         // nodes on the left have a negative index
         if (node.left) myQ.push([node.left, i-1]);
         // nodes on the right have a positive index
@@ -32,8 +31,5 @@ var verticalOrder = function(root) {
     // get the values of each of the indexes in order
     // from negative to positive
     let res2 = res1.map(k => myMap[k]);
-    console.log("res1: ", res1);
-    console.log("res2: ", res2)
-    return res2
-    // return Object.keys(myMap).sort((k1, k2) => k1 - k2).map(k => myMap[k]);
+    return res2;
 };
