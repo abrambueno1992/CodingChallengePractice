2f68482e5ca2455fa647aa6504153c050c2213d6
Abraham Bueno <abueno@localhost.localdomain>
2019-01-31 11:38:08 -0600

    Made changes to make it simpler to comprehend.

--- NumberOfIslands.js
+++ NumberOfIslands.js
@@ -4,45 +4,47 @@
  */
 var numIslands = function(grid) {
     let count = 0;
-    let tracker = 0;
+    let row = 0;
     
-    while (grid[tracker] !== undefined) {
-        console.log('current array', grid[tracker])
+    while (grid[row] !== undefined) {
+        console.log('current array', grid[row])
         let inner = 0;
         let current = 0;
-        for (let i = 0; grid[tracker].length > i;i++) {
-            if(grid[tracker][i] === "1") {
-                // current reassigned grid[trakcer][i] ="2"
-                grid[tracker][i] = "2";
+        for (let column = 0; grid[row].length > column;column++) {
+            // row index from outside 'while' loop
+            // column index from 'for' loop
+            if(grid[row][column] === "1") {
+                // current reassigned grid[row][i] ="2"
+                grid[row][column] = "2";
                 // recursive
-                isLand(grid, tracker, i );
+                isLand(grid, row, column );
                 count++;
             }
             
         }
-        tracker++;
+        row++;
     }
     return count
     console.log('count: ', count)
     
 };
-function isLand(matrix, i, j) {
+function isLand(matrix, row, column) {
     // recursion, goes through all paths
-    // i vertical, j horizontal
-    // i++ down, i-- up, j++ right, j-- left
-    checkXY(matrix, i, j+1); // right
-    checkXY(matrix, i, j-1); // left
-    checkXY(matrix, i-1, j); // top
-    checkXY(matrix, i+1, j); // bottom
+    // row vertical, column horizontal
+    // row++ down, row-- up, column++ right, column-- left
+    checkXY(matrix, row, column+1); // right
+    checkXY(matrix, row, column-1); // left
+    checkXY(matrix, row-1, column); // top
+    checkXY(matrix, row+1, column); // bottom
 }
 
-function checkXY(matrix, i, j) {
-    // if matrix[i] is not undefined / defined
-    // and matrix[i][j] === "1"
-    // matrix[i][j] = "0", reassigned
-    // call isLand(matrix, i, j) => recursion
- if(matrix[i] && matrix[i][j] == "1") {
-  matrix[i][j] = "0";
-  isLand(matrix, i, j);
+function checkXY(matrix, row, column) {
+    // if matrix[row] is not undefined / defined
+    // and matrix[row][column] === "1"
+    // matrix[row][column] = "0", reassigned
+    // call isLand(matrix, row, column) => recursion
+ if(matrix[row] && matrix[row][column] == "1") {
+  matrix[row][column] = "0";
+  isLand(matrix, row, column);
  }
 }
