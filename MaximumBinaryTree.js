6ad26de23e6bd0b3028240eb302e2651d2f8376b
Abraham Bueno <abueno@localhost.localdomain>
2019-01-30 22:17:23 -0600

    Added comments to explain

--- MaximumBinaryTree.js
+++ MaximumBinaryTree.js
@@ -12,20 +12,32 @@
 
 
 var max=function(nums){
+    // initial max is first value
     let max=nums[0],max_ind=0;
     nums.map((value,index)=>{
+        // reassign max values
         if(max<value) {
             max=value;
             max_ind=index;
         }
     });
+    // return max index
     return max_ind;
 }
 var constructMaximumBinaryTree = function(nums) {
+    // if no length, return null
     if(!nums.length) return null;
+    // get max index of nums array
     let max_ind=max(nums);
+    // tree with maximum node value at root
     let tree=new TreeNode(nums[max_ind]);
+    // left nodes with smaller node values
+    // from 0 index to max_ind, slice
     tree.left=constructMaximumBinaryTree(nums.slice(0,max_ind));
+    // right nodes with larger node values
+    // from max_ind + 1 to the end, slice
     tree.right=constructMaximumBinaryTree(nums.slice(max_ind+1));
+    
+    // return tree after adding nodes
     return tree;
 };
