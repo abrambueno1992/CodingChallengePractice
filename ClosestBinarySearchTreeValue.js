b94f97d3d09e42cdcca3865084cee4f9f045fc67
Abraham Bueno <abueno@localhost.localdomain>
2019-01-30 21:24:29 -0600

    Better comments on code

--- ClosestBinarySearchTreeValue.js
+++ ClosestBinarySearchTreeValue.js
@@ -18,15 +18,26 @@ var closestValue = function(root, target) {
         {
             if(target < root.val)
                 {
+                    // return the smallest value, root.val or larger
                     larger = Math.min(root.val, larger);
+                    // root changed to root.left
+                    // left nodes have lower values
                     root = root.left;
                 }
             else
                 {
+                    // value is less than target
+                    // return the maximum value, root.val or smaller
                     smaller = Math.max(root.val, smaller);
+                    // root changed to root.right
+                    // right nodes have higher values
                     root = root.right;
                 }
         }
+    // if larger < target
+    // if target > smaller
+    // return larger
     if(larger-target < target-smaller) return larger;
+    // else if larger > target, larger is above the target
     else return smaller;
 };
